82605ecc13b8ae4c2c9a0ad9ff7d14de322b5848
Xinlong Yi <xinlong1996@gmail.com>
2021-05-06 09:37:02 -0700

    try to fix issue

--- include/object.h
+++ include/object.h
@@ -12,13 +12,13 @@ class Shader;
 
 class Object {
 public:
-  __device__  Object(){};
-  __device__  ~Object(){};
-  __device__  virtual void Intersection(Ray &ray, Hit &hit) const = 0;
-  __device__  virtual vec3 Norm(vec3 &point) const = 0;
+  __device__ Object(){};
+  __device__ ~Object(){};
+  __device__ virtual void Intersection(Ray &ray, Hit &hit) const = 0;
+  __device__ virtual vec3 Norm(vec3 &point) const = 0;
 
   Shader *shader;
-  static const double small_t = 1e-4;
+  double small_t = 1e-4;
 };
 
 #endif
